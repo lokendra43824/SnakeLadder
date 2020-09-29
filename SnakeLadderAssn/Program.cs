8d154949a9f7bce4198f38b9f5698ff0be12ba00
Lokendra <lokilokendra12345@gmail.com>
2020-09-29 21:44:04 +0530

    [lokendra] added a code to play a game with two problem

--- SnakeLadderAssn/Program.cs
+++ SnakeLadderAssn/Program.cs
@@ -5,6 +5,67 @@ namespace SnakeLadder
     class Program
     {
 
+        const int NO_Play = 0;
+        const int Ladder = 1;
+        const int Snake = 2;
+
+        public static int PlayGame(String player, int prev_postion)
+        {
+            // variables
+
+            int position = prev_postion;
+            int next_position = 0;
+            int play_option = 0;
+            bool ladder = true;
+
+            // while ladder is true player continues the game 
+            // or he quits
+
+            while (ladder == true)
+            {
+                next_position = GetPositon(0, 7);
+
+                play_option = GetPositon(0, 3);
+
+                switch (play_option)
+                {
+                    case NO_Play:
+                        Console.Out.WriteLine(player + " NO_PLAY, ur postion is " + position);
+                        ladder = false;
+                        break;
+                    case Ladder:
+                        position += next_position;
+                        ladder = true;
+                        if (position <= 100)
+                        {
+                            Console.Out.WriteLine(player + " Moved forward to " + position);
+
+                            break;
+                        }
+                        else
+                        {
+                            position -= next_position;
+                            Console.Out.WriteLine(player + " Moved forward to " + position);
+                        }
+                        break;
+                    case Snake:
+                        position -= next_position;
+                        if (position <= 0)
+                        {
+                            position = 0;
+
+                        }
+                        Console.Out.WriteLine(player + " Moved backward to " + position);
+                        ladder = false;
+                        break;
+                    default:
+                        ladder = false;
+                        break;
+                }
+            }
+
+            return position;
+        }
         public static int GetPositon(int start, int end)
         {
             Random rand = new Random();
@@ -14,59 +75,36 @@ namespace SnakeLadder
         static void Main(string[] args)
         {
             String player1 = "Player1";
-
-            //Constants
-
-            const int NO_Play = 0;
-            const int Ladder = 1;
-            const int Snake = 2;
+            String player2 = "Player2";
 
 
             //variables
 
-            int position = 0;
-            int next_position = 0;
-            int play_option = 0;
-            int count1 = 0;
+            int position1 = 0;
+
+            int position2 = 0;
 
 
-
-
-            while (position != 100)
+            while ((position1 != 100) || (position2 != 100))
             {
-                next_position = GetPositon(0, 7);
+                position1 = PlayGame(player1, position1);
 
-                play_option = GetPositon(0, 3);
-
-                switch (play_option)
+                if (position1 == 100)
                 {
-                    case NO_Play:
-                        Console.Out.WriteLine("NO_PLAY, ur postion is " + position);
-                        break;
-                    case Ladder:
-                        position += next_position;
-                        if (position > 100)
-                        {
-                            position -= next_position;
-                            break;
-                        }
-                        Console.Out.WriteLine("Moved forward to " + position);
-                        break;
-                    case Snake:
-                        position -= next_position;
-                        if (position <= 0)
-                        {
-                            position = 0;
-                        }
-                        Console.Out.WriteLine("Moved backward to " + position);
-                        break;
-                    default:
-                        break;
+                    break;
                 }
-                count1++;
+                position2 = PlayGame(player2, position2);
             }
 
-            Console.Out.WriteLine("The dice is rolled " + count1 + " times to reach 100");
+            if (position1 == 100)
+            {
+                Console.Out.WriteLine("Player1 won the game");
+            }
+            else
+            {
+                Console.Out.WriteLine("Player2 won the game");
+            }
+
         }
     }
 }

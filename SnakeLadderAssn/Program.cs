511d3d95cceb1f7b8f17e65e818f6ef70e5a2f8c
Lokendra <lokilokendra12345@gmail.com>
2020-09-29 18:47:48 +0530

    [lokendra]Refactor.added a code for uc1

--- SnakeLadderAssn/Program.cs
+++ SnakeLadderAssn/Program.cs
@@ -6,7 +6,17 @@ namespace SnakeLadderAssn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Out.WriteLine("Welcome to Snake Ladder Problem");
+
+            //Constants
+
+            const int INITIAL_POS = 0;
+
+            //variables
+
+            int position = 0;
+
+            Console.Out.WriteLine("Start position is: " + INITIAL_POS);
         }
     }
 }
